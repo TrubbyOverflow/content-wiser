5564a5358e799be83f9889924892f17303946d11
Laury Bueno <laury.bueno@gmail.com>
2020-03-28 20:59:07 -0300

    [Import] Save data on the API

--- ui/pages/import.js
+++ ui/pages/import.js
@@ -1,35 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useMutation } from "@apollo/react-hooks"
+import { gql } from "apollo-boost"
 import withData from '../lib/apollo'
 
 import styles from './import.module.css'
 
-class Import extends React.Component {
-    constructor(props) {
-      super(props)
-      this.handleSubmit = this.handleSubmit.bind(this)
-      this.state = {
-          url: ''
-      }
+
+const CREATE_RSS = gql`
+  mutation CreateRSS($page: String!) {
+    CreateRss(page: $page){
+        page
+    }
+  }
+`
+
+const Import = () => {
+    const [url, setURL] = useState('')
+
+    const [createRSS, { error, data }] = useMutation(CREATE_RSS, {
+      variables: { page: url }
+    });
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+
+      console.log(url)
+      createRSS()
     }
 
-    handleSubmit(e) {
-      e.preventDefault();
-
-      // TODO: send data to the API
-      console.log(this.state.url)
-    }
-
-    render() {
-      return (
-        <div id={styles.import_page}>
-          <form className={styles.feed_form} onSubmit={this.handleSubmit}>
-            <div className={styles.text}>Informe abaixo a URL do feed que você quer adicionar à sua conta</div>
-            <input type="text" className={styles.feed_input} onChange={e => this.setState({url: e.target.value})} />
-            <button className={styles.feed_button} type="submit">Salvar</button>
-          </form>
-        </div>
-      )
-    }
+    return (
+      <div id={styles.import_page}>
+        <form className={styles.feed_form} onSubmit={handleSubmit}>
+          <div className={styles.text}>Informe abaixo a URL do feed que você quer adicionar à sua conta</div>
+          <input type="text" className={styles.feed_input} onChange={e => setURL(e.target.value)} />
+          <button className={styles.feed_button} type="submit">Salvar</button>
+        </form>
+      </div>
+    )
 }
 
 export default withData(Import)
